feb2e529e8e66070925e45cef0647d6f2a51a51c
RobjeGamer <robbe.verberck@gmail.com>
2025-06-05 11:30:03 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
+loops.everyInterval(1000, function () {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 210)
+    basic.pause(2000)
+})
+basic.forever(function () {
+	
 })

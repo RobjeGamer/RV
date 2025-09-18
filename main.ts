68e7191a298b81d180fbdc5f3f10edcd5100cd4d
RobjeGamer <robbe.verberck@gmail.com>
2025-09-18 12:44:47 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
-let vooruit = 0
-let achteruit = 0
-let links = 0
-let rechts = 0
-let ir_waarde = 0
-IR.IR_init()
-basic.forever(function () {
-    ir_waarde = IR.IR_read()
-    rechts = 197
-    links = 69
-    achteruit = 5
-    vooruit = 133
-})
+let rechts = 197
+let links = 69
+let achteruit = 5
+let vooruit = 133
+let ir_waarde = IR.IR_read()
 basic.forever(function () {
     basic.showNumber(ir_waarde)
+})
+basic.forever(function () {
+    if (ir_waarde == vooruit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
 })

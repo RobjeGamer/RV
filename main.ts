653fb0ad09df0d701eef5db0a71b641c9bfa5c39
RobjeGamer <robbe.verberck@gmail.com>
2025-09-18 10:25:22 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
-loops.everyInterval(1000, function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 210)
-    basic.pause(2000)
+let vooruit = 0
+let achteruit = 0
+let links = 0
+let rechts = 0
+let ir_waarde = 0
+IR.IR_init()
+basic.forever(function () {
+    ir_waarde = IR.IR_read()
+    rechts = 197
+    links = 69
+    achteruit = 5
+    vooruit = 133
 })
 basic.forever(function () {
-	
+    basic.showNumber(ir_waarde)
 })
